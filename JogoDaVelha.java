0a9c4815b0b61027e0cf2a50538a88cb19b19cf8
Biancanoronha <biancateixeirawtn@gmail.com>
2026-04-30 21:16:02 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -2,12 +2,24 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class JogoDaVelha {
+
+    private Jogador jogador1;
+    private Jogador jogador2;
+    private Tabuleiro tabuleiro;
+
+    public JogoDaVelha(Jogador jogador1, Jogador jogador2, Tabuleiro tabuleiro) {
+        this.jogador1 = jogador1;
+        this.jogador2 = jogador2;
+        this.tabuleiro = tabuleiro;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
         char simboloHumano;
         char simboloMaquina;
+
         while (true) {
             System.out.print("Escolha seu símbolo (X ou O): ");
             String input = sc.next().toUpperCase();
@@ -16,13 +28,23 @@ public class JogoDaVelha {
                 simboloMaquina = (simboloHumano == 'X') ? 'O' : 'X';
                 break;
             } else {
-                System.out.println("Símbolo inválido. Por favor, escolha 'X' ou 'O'.");
+                System.out.println("Símbolo inválido. Tente novamente.");
             }
         }
 
         Jogador jogador1 = new Jogador("Jogador 1", simboloHumano);
         Jogador jogador2 = new Jogador("Jogador 2", simboloMaquina);
         Tabuleiro tabuleiro = new Tabuleiro();
+
+        JogoDaVelha jogo = new JogoDaVelha(jogador1, jogador2, tabuleiro);
+        jogo.iniciar();
+
+        sc.close();
+    }
+    
+    private void iniciar() {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
 
         Jogador atual = jogador1;
         boolean jogoAcabou = false;
@@ -45,10 +67,10 @@ public class JogoDaVelha {
                         if (tabuleiro.fazerJogada(linha, coluna, atual.getSimbolo())) {
                             jogadaValida = true;
                         } else {
-                            System.out.println("Posição ocupada ou inválida! Tente novamente.");
+                            System.out.println("Posição ocupada ou inválida!");
                         }
                     } else {
-                        System.out.println("Coordenadas fora do limite (0-2)! Tente novamente.");
+                        System.out.println("Fora do limite!");
                     }
                 }
             } else {
@@ -63,15 +85,11 @@ public class JogoDaVelha {
 
             if (tabuleiro.verificarVitoria(atual.getSimbolo())) {
                 tabuleiro.exibir();
-                if (atual.getNome().equals("Jogador 1")) {
-                    System.out.println("O jogador 1 ganhou");
-                } else {
-                    System.out.println("O jogador 2 ganhou");
-                }
+                System.out.println(atual.getNome() + " ganhou!");
                 jogoAcabou = true;
             } else if (tabuleiro.cheio()) {
                 tabuleiro.exibir();
-                System.out.println("O jogo terminou empatado.");
+                System.out.println("Empate!");
                 jogoAcabou = true;
             } else {
                 atual = (atual == jogador1) ? jogador2 : jogador1;
@@ -79,6 +97,5 @@ public class JogoDaVelha {
         }
 
         System.out.println("Criado por Bianca Teixeira");
-        sc.close();
     }
 }
